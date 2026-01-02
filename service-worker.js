606f74894ead10d7b742ba67903640cb2ff23264
ivantone245-droidIvan <ivantone245@gmail.com>
2026-01-02 22:39:55 +0300

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = 'ivan-gaming-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
